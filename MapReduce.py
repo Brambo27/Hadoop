2991eb93e733b04b75dd0563fe684770704c313e
Bram Bos <brambos27@gmail.com>
2022-07-14 20:52:17 +0200

    Initial Commit

---
+++ MapReduce.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
+        ]
+
+    def mapper(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
